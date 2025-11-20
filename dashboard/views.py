6b1eb15824b4c1dd267c3666ba59d245ca873e24
princerusel <princeruselbstamaria@gmail.com>
2025-11-20 21:32:53 +0800

    Revert "Add accounts app with authentication views and templates; update dashboard view and styles"
    not wortking
    This reverts commit 2cf09984e76771a90e32feb33592b7b99e044ac7.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+# Create your views here.
 def welcome_view(request):
-    return render(request, 'dashboard/index.html')
+    return HttpResponse('rezo ng mga pogi')
--- rezo/urls.py
+++ rezo/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from dashboard.views import welcome_view
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', welcome_view, name='welcome'),
-    path('accounts/', include('accounts.urls'))
+    path('', welcome_view, name='welcome')
 ]
