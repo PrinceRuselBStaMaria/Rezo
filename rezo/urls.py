796a92937bac5ce281c1c3fbc3743a0eeeb4f40b
princerusel <princeruselbstamaria@gmail.com>
2025-11-22 02:23:54 +0800

    Add image field to Asset model and update asset list template for image display

--- rezo/urls.py
+++ rezo/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from accounts import views as accounts_views
 from inventory.views import home, asset_list
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('inventory/', include('inventory.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
